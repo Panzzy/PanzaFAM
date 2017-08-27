30d97dfa6e9a3d8811f59fca7d2ca24e8e7625a8
Benny Kwok <kwokb1@gmail.com>
2017-08-27 05:17:49 +0000

    Add miner files

---
+++ start.bash
@@ -0,0 +1,7 @@
+export GPU_FORCE_64BIT_PTR=0
+export GPU_MAX_HEAP_SIZE=100
+export GPU_USE_SYNC_OBJECTS=1
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_SINGLE_ALLOC_PERCENT=100
+
+./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0xE8eb99FAB26646ee5097006F7c0ca1bF84aF622d.PanzaFAM1/kwokb1@gmail.com -epsw x -mode 1 -ftime 10
